4c815eac8c5cf2b84b2ced7f9c1652eecfb72af2
bmhuyquoc104 <56634994+bmhuyquoc104@users.noreply.github.com>
2022-03-25 20:31:00 +0700

    22. Finish the functionality for upvote and downvote

--- src/components/Comment/Comment.jsx
+++ src/components/Comment/Comment.jsx
@@ -1,8 +1,121 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 import { StyledComment, StyledReply } from "./Comment.styled";
 import CurrentUserComment from "./CurrentUserComment/CurrentUserComment";
 import imagesResource from "../../assets/images";
 import { StyledFlexContainer } from "../Flex/Flex.styled";
+
+// Function to render a button and do a upvote and downvote function for that button
+// Using the current score as the props from Comment component
+const RenderButton = ({score}) => {
+  // Initiallize the state for the button
+  const initialState = {
+    currentVote: score,
+    isPlusActive: false,
+    isMinusActive: false,
+  };
+
+  // Function reducer for useReducer
+  const voteReducer = (state, action) => {
+    switch (action.type) {
+      case "UPVOTEPLUS":
+        return {
+          currentVote: state.currentVote + 1,
+          isPlusActive: state.isMinusActive ? false : true,
+          isMinusActive: false,
+        };
+      case "UPVOTEMINUS":
+        return {
+          currentVote: state.currentVote - 1,
+          isPlusActive: false,
+          isMinusActive: state.isPlusActive ? false : true,
+        };
+      case "DOWNVOTEMINUS":
+        return {
+          currentVote: state.currentVote + 1,
+          isPlusActive: false,
+          isMinusActive: false,
+        };
+      case "DOWNVOTEPLUS":
+        return {
+          currentVote: state.currentVote - 1,
+          isPlusActive: false,
+          isMinusActive: false,
+        };
+      default:
+        return state;
+    }
+  };
+
+  // Use the useReducer hook to render the button and do the upvote and downvote function base on the current state
+  const [voteState, dispatch] = useReducer(voteReducer, initialState);
+
+  
+  // Render the button base on the current state
+  return(
+ <div className="comment-button">
+   {!voteState.isPlusActive ? (
+    //  If the button is not active, render the plus button with the upvote function
+     <button
+       onClick={() => dispatch({ type: "UPVOTEPLUS" })}
+       className="plus"
+     >
+       <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
+         <path
+           fill="#C5C6EF"
+           d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
+         />
+       </svg>
+     </button>
+   ) : (
+      // If the button is active, render the plus button with the downvote function
+     <button
+       onClick={() => dispatch({ type: "DOWNVOTEPLUS" })}
+       className="plus"
+     >
+       <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
+         <path
+           fill="hsl(238, 40%, 52%)"
+           d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
+         />
+       </svg>
+     </button>
+   )}
+   <div className="score">
+     <p>{voteState.currentVote}</p>
+   </div>
+   {!voteState.isMinusActive ? (
+      // If the button is not active, render the minus button with the upvoteMinus function
+     <button
+       onClick={() => dispatch({ type: "UPVOTEMINUS" })}
+       className="minus"
+     >
+       <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg">
+         <path
+           fill="#C5C6EF"
+           d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
+         />
+       </svg>
+     </button>
+   ) : (
+      // If the button is active, render the minus button with the downvoteMinus function
+     <button
+       onClick={() => dispatch({ type: "DOWNVOTEMINUS" })}
+       className="minus"
+     >
+       <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg">
+         <path
+           fill="hsl(238, 40%, 52%)"
+           d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
+         />
+       </svg>
+     </button>
+   )}
+ </div>
+  )
+};
+
+
+
 const Comment = ({
   score,
   content,
@@ -14,29 +127,20 @@ const Comment = ({
   currentUser,
 }) => {
   const [isReplyingComment, setReplyingComment] = useState(false);
-  const [isUpdating,setUpdating] = useState(false);
-  const [isReplyingAReply,setReplyingAReply] = useState(false);
+  const [isUpdating, setUpdating] = useState(false);
+  const [isReplyingAReply, setReplyingAReply] = useState(false);
+
+  
+  
+
   return (
     <>
+      {/* render only a comment without any replies */}
       {replies.length <= 0 ? (
         <StyledFlexContainer>
+          {/* render all the comments */}
           <StyledComment>
-            <div className="comment-button">
-              <button className="plus">
-                <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" />
-                </svg>
-              </button>
-              <div className="score">
-                <p>{score}</p>
-              </div>
-              <button className="minus">
-                <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" />
-                </svg>
-              </button>
-            </div>
-
+          <RenderButton score = {score}/>
             <div className="comment-body">
               <div className="comment-user">
                 <div className="left-side">
@@ -48,7 +152,10 @@ const Comment = ({
                 </div>
                 <div className="right-side">
                   <img src={imagesResource.iconReply} alt="A reply icon" />
-                  <button onClick={() => setReplyingComment(!isReplyingComment)}>
+                  {/* open the reply box when click */}
+                  <button
+                    onClick={() => setReplyingComment(!isReplyingComment)}
+                  >
                     Reply
                   </button>
                 </div>
@@ -58,6 +165,7 @@ const Comment = ({
               </div>
             </div>
           </StyledComment>
+          {/* only open the reply box when reply is clicked */}
           {isReplyingComment && (
             <CurrentUserComment
               png={currentUser.image.png}
@@ -66,24 +174,13 @@ const Comment = ({
           )}
         </StyledFlexContainer>
       ) : (
+        // render all the comments with the replies
         <StyledFlexContainer>
           <StyledComment>
-            <div className="comment-button">
-              <button className="plus">
-                <svg width="11" height="11" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" />
-                </svg>{" "}
-              </button>
-              <div className="score">
-                <p>{score}</p>
-              </div>
-              <button className="minus">
-                <svg width="11" height="3" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" />
-                </svg>{" "}
-              </button>
-            </div>
+            {/* comment button */}
+            <RenderButton score = {score}/>
 
+            {/* comment body */}
             <div className="comment-body">
               <div className="comment-user">
                 <div className="left-side">
@@ -109,7 +206,7 @@ const Comment = ({
               </div>
             </div>
           </StyledComment>
-
+          {/* render all replies from that comment  */}
           <StyledReply>
             <div className="decoration-container">
               <div className="decoration"></div>
@@ -119,29 +216,7 @@ const Comment = ({
                 currentUser.username !== reply.user.username ? (
                   <StyledFlexContainer key={reply.id}>
                     <StyledComment>
-                      <div className="comment-button">
-                        <button className="plus">
-                          <svg
-                            width="11"
-                            height="11"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" />
-                          </svg>{" "}
-                        </button>
-                        <div className="score">
-                          <p>{reply.score}</p>
-                        </div>
-                        <button className="minus">
-                          <svg
-                            width="11"
-                            height="3"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" />
-                          </svg>
-                        </button>
-                      </div>
+                    <RenderButton score = {score}/>
 
                       <div className="comment-body">
                         <div className="comment-user">
@@ -160,7 +235,12 @@ const Comment = ({
                               src={imagesResource.iconReply}
                               alt="A reply icon"
                             />
-                            <button onClick={() => setReplyingAReply(!isReplyingAReply)}>
+                            {/* open a reply box for each reply in a comment */}
+                            <button
+                              onClick={() =>
+                                setReplyingAReply(!isReplyingAReply)
+                              }
+                            >
                               Reply
                             </button>
                           </div>
@@ -172,6 +252,7 @@ const Comment = ({
                         </div>
                       </div>
                     </StyledComment>
+                    {/* render a reply box when the reply button is clicked */}
                     {isReplyingAReply && (
                       <CurrentUserComment
                         png={currentUser.image.png}
@@ -180,31 +261,11 @@ const Comment = ({
                     )}
                   </StyledFlexContainer>
                 ) : (
+                  // render a reply from current user -> change the layout
                   <StyledFlexContainer key={reply.id}>
                     <StyledComment>
-                      <div className="comment-button">
-                        <button className="plus">
-                          <svg
-                            width="11"
-                            height="11"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" />
-                          </svg>{" "}
-                        </button>
-                        <div className="score">
-                          <p>{reply.score}</p>
-                        </div>
-                        <button className="minus">
-                          <svg
-                            width="11"
-                            height="3"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" />
-                          </svg>
-                        </button>
-                      </div>
+                    <RenderButton score = {score}/>
+
 
                       <div className="comment-body">
                         <div className="comment-user">
@@ -234,7 +295,10 @@ const Comment = ({
                                 src={imagesResource.iconEdit}
                                 alt="A edit icon"
                               />
-                              <button onClick={() => setUpdating(!isUpdating)}>Edit</button>
+                              {/* a button to open the update box */}
+                              <button onClick={() => setUpdating(!isUpdating)}>
+                                Edit
+                              </button>
                             </div>
                           </div>
                         </div>
@@ -246,7 +310,7 @@ const Comment = ({
                         </div>
                       </div>
                     </StyledComment>
-                   
+                    {/* open the update box for update the comment from current user */}
                     {isUpdating && (
                       <CurrentUserComment
                         png={currentUser.image.png}
@@ -258,12 +322,14 @@ const Comment = ({
               )}
             </div>
           </StyledReply>
+          {/* open the reply box for the comment that has reply */}
           {isReplyingComment && (
             <CurrentUserComment
               png={currentUser.image.png}
               buttonRole="reply"
             />
           )}
+          {/* the box for current user to add new comment */}
           <CurrentUserComment png={currentUser.image.png} buttonRole="send" />
         </StyledFlexContainer>
       )}
